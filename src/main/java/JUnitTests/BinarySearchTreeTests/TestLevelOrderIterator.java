300afc8d3f882a28916f94717f8c203e060b20e8
Nathan Darrah <nmd001@lvc.edu>
2019-02-13 09:52:07 -0500

    Commented unnecessary tests, finished some unfinished tests

--- src/main/java/JUnitTests/BinarySearchTreeTests/TestLevelOrderIterator.java
+++ src/main/java/JUnitTests/BinarySearchTreeTests/TestLevelOrderIterator.java
@@ -1,7 +1,10 @@
 package JUnitTests.BinarySearchTreeTests;
 
+import BinarySearchTree.LevelOrderIterator;
 import BinarySearchTree.Node;
 import org.junit.Test;
+
+import java.util.Iterator;
 
 import static org.junit.Assert.*;
 
@@ -10,17 +13,24 @@ public class TestLevelOrderIterator {
     Node<Integer> n1 = new Node<Integer>(1, 2);
     Node<Integer> n2 = new Node<Integer>(2, 1);
     Node<Integer> n3 = new Node<Integer>(3, 3);
+    LevelOrderIterator<Integer> it;
 
     public void setup() {
-
+        n1.setLeft(n2);
+        n1.setRight(n3);
+        it = new LevelOrderIterator<Integer>(n1);
     }
 
     @Test
     public void hasNext() throws Exception {
+        setup();
+        assertTrue(it.hasNext());
     }
 
     @Test
     public void get() throws Exception {
+        setup();
+        assertEquals(Integer.valueOf(1), it.get());
     }
 
     @Test

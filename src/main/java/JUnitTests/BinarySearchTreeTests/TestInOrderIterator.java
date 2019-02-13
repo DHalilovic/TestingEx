175e29db35032143da8964b78df122212c2f97ac
Nathan Darrah <nmd001@lvc.edu>
2019-02-13 09:30:35 -0500

    Commented unnecessary tests, finished some unfinished tests

--- src/main/java/JUnitTests/BinarySearchTreeTests/TestInOrderIterator.java
+++ src/main/java/JUnitTests/BinarySearchTreeTests/TestInOrderIterator.java
@@ -1,24 +1,36 @@
 package JUnitTests.BinarySearchTreeTests;
 
+import BinarySearchTree.InOrderIterator;
+import BinarySearchTree.Node;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class TestInOrderIterator {
+
+    Node<Integer> n1 = new Node<Integer>(1, 2);
+    Node<Integer> n2 = new Node<Integer>(2, 1);
+    Node<Integer> n3 = new Node<Integer>(3, 3);
+    InOrderIterator<Integer> it;
+
+    public void setup() {
+        n1.setLeft(n2);
+        n1.setRight(n3);
+        it = new InOrderIterator<Integer>(n1);
+    }
+
     @Test
     public void get() throws Exception {
+        assertEquals(Integer.valueOf(1), it.get());
     }
 
     @Test
     public void hasNext() throws Exception {
+        assertTrue(it.hasNext());
     }
 
     @Test
     public void next() throws Exception {
+        assertEquals(Integer.valueOf(1), it.next());
     }
-
-    @Test
-    public void remove() throws Exception {
-    }
-
 }

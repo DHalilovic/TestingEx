1690c2bd6a106464546b0a9ed1c516352c13c018
Nathan Darrah <nmd001@lvc.edu>
2019-02-13 09:14:40 -0500

    Commented unnecessary tests

--- src/main/java/JUnitTests/BinarySearchTreeTests/TestClassicOrdering.java
+++ src/main/java/JUnitTests/BinarySearchTreeTests/TestClassicOrdering.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestClassicOrdering {
+
+    //Not a public class
+
     @Test
     public void compare() throws Exception {
     }

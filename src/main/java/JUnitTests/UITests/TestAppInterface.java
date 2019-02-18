f838dad11e62b5d6c45daac23364b8b3d6030c94
Erik Kaufman <erik.kaufman@live.com>
2019-02-18 01:59:40 -0500

    Added log4j2.xml. Got logging to file to work.

--- src/main/java/JUnitTests/UITests/TestAppInterface.java
+++ src/main/java/JUnitTests/UITests/TestAppInterface.java
@@ -1,35 +1,45 @@
 package JUnitTests.UITests;
 
+import BinarySearchTree.BinarySearchTree;
 import Database.*;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
 //import static org.junit.Assert.*;
 //import static org.mockito.Mockito.*;
 
 public class TestAppInterface extends TestInterface {
 
-    public class MockitoTest  {
+    //@Test
+    //public void testAdd() throws Exception {
+//        BinarySearchTree mockTree = Mockito.mock(BinarySearchTree.class);
+//        when(mockTree.add(id))
+//        MyClass clazz = new MyClass(mockTree);
+//    }
 
-        @Mock
-        Database databaseMock;
-
-        @Rule
-        public MockitoRule mockitoRule = MockitoJUnit.rule();
-
-        @Test
-        public void testQuery()  {
-//            ClassToTest t  = new ClassToTest(databaseMock);
-//            boolean check = t.query("* from t");
-//            assertTrue(check);
-//            verify(databaseMock).query("* from t");
-        }
-    }
+//    public class MockitoTest  {
+//
+//        @Mock
+//        Database databaseMock;
+//
+//        @Rule
+//        public MockitoRule mockitoRule = MockitoJUnit.rule();
+//
+//        @Test
+//        public void testQuery()  {
+////            ClassToTest t  = new ClassToTest(databaseMock);
+////            boolean check = t.query("* from t");
+////            assertTrue(check);
+////            verify(databaseMock).query("* from t");
+//        }
+//    }
 
 }
